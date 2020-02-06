1249f59bf3447fc88828f596e34b1c6d50e1deee
Javier Calvarro Nelson <jacalvar@microsoft.com>
2020-02-06 01:21:19 -0800

    Move sample to use in memory db

--- src/Components/Blazor/testassets/Wasm.Authentication.Client/Program.cs
+++ src/Components/Blazor/testassets/Wasm.Authentication.Client/Program.cs
@@ -10,21 +10,7 @@ namespace Wasm.Authentication.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            builder.Services.AddOidcAuthentication(options =>
-            {
-                options.ProviderOptions.Authority = "https://accounts.google.com/";
-                options.ProviderOptions.ClientId = "1048855053233-16qpvegahk2leksb6q2hheoas56pfib9.apps.googleusercontent.com";
-
-            });
-
-            //builder.Services.AddMsalSpaAuthentication(options =>
-            //{
-            //    var authentication = options.ProviderOptions.Authentication;
-            //    authentication.Authority = "https://login.microsoftonline.com/7e511586-66ec-4108-bc9c-a68dee0dc2aa";
-            //    authentication.ClientId = "ae13253e-6630-48c2-9f99-52ec618d9c4c";
-
-            //    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://BlazorWasmAadAPI/All");
-            //});
+            builder.Services.AddApiAuthorization();
 
             builder.RootComponents.Add<App>("app");
 
